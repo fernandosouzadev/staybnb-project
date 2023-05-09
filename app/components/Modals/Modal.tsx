9462c8facead9fbdaae8644c2a361e53e1ef52ab
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-09 17:48:50 -0300

    Feat: Created component Modal

---
+++ app/components/Modals/Modal.tsx
@@ -0,0 +1,111 @@
+'use client'
+
+import { ReactElement, useCallback, useEffect, useState } from 'react'
+import { IoMdClose } from 'react-icons/io'
+import { Button } from '../Button'
+
+interface ModalProps {
+  isOpen?: boolean
+  onClose: () => void
+  onSubmit: () => void
+  title?: string
+  body?: ReactElement
+  footer?: ReactElement
+  actionlabel: string
+  disabled?: boolean
+  secondaryAction?: () => void
+  secondaryActionLabel?: string
+}
+
+export function Modal({
+  isOpen,
+  onSubmit,
+  onClose,
+  title,
+  body,
+  footer,
+  actionlabel,
+  disabled,
+  secondaryAction,
+  secondaryActionLabel,
+}: ModalProps) {
+  const [showModal, setShowModal] = useState(isOpen)
+
+  useEffect(() => {
+    setShowModal(isOpen)
+  }, [isOpen])
+
+  const handleClose = useCallback(() => {
+    if (disabled) {
+      return
+    }
+
+    setShowModal(false)
+    setTimeout(() => {
+      onClose()
+    }, 300)
+  }, [disabled, onClose])
+
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return
+    }
+    onSubmit()
+  }, [onSubmit, disabled])
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) {
+      return
+    }
+    secondaryAction()
+  }, [disabled, secondaryAction])
+
+  if (!isOpen) {
+    return null
+  }
+
+  return (
+    <>
+      <div className="justify-center items-center flex  overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+        <div className="relative w-full md:w-4/6 lg:w-4/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+          <div
+            className={`translate duration-300 h-full ${
+              showModal ? 'translate-y-0' : `translate-y-full`
+            } ${showModal ? 'opacity-100' : `opacity-0`}`}
+          >
+            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg relative flex flex-col w-full bg-white outline-none focus:outline-none overflow-y-auto">
+              <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
+                <button
+                  onClick={handleClose}
+                  className="p-1 border0 hover:opacity-70 transition absolute left-9"
+                >
+                  <IoMdClose size={18} />
+                </button>
+                <h1 className="text-lg font-semibold">{title}</h1>
+              </div>
+              <div className="relative p-6 flex-auto">{body}</div>
+              <div className="flex flex-col gap-2 p-6">
+                <div className="flex flex-row items-center gap-4 w-full">
+                  {secondaryActionLabel && secondaryAction && (
+                    <Button
+                      label={secondaryActionLabel}
+                      disabled={disabled}
+                      onClick={handleSecondaryAction}
+                      outline
+                    />
+                  )}
+                  <Button
+                    label={actionlabel}
+                    disabled={disabled}
+                    onClick={handleSubmit}
+                  />
+                </div>
+                {footer}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
