cbf36afd081947f243555697ffa4fc70bd89f23b
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-09 17:50:47 -0300

    Feat: adding logic to open modal on MenuItem

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,7 +1,11 @@
 'use client'
+
+import { useAppDispatch } from '@/app/redux/hooks'
+import { openRegisterModal } from '@/app/redux/registerModal/actions'
 import { useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { Avatar } from '../Avatar'
+import { RegisterModal } from '../Modals/RegisterModal'
 import { MenuItem } from './MenuItem'
 
 export function UserMenu() {
@@ -9,35 +13,42 @@ export function UserMenu() {
   const toggleOpen = useCallback(() => {
     setIsOpen((state) => !state)
   }, [])
+  const dispatch = useAppDispatch()
+  function handleOpenModalRegister() {
+    dispatch(openRegisterModal())
+  }
   return (
-    <div className="relative">
-      <div className="flex flex-row items-center gap-3">
-        <div
-          onClick={() => {}}
-          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-        >
-          Airbnb your home
-        </div>
-        <div
-          onClick={toggleOpen}
-          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-        >
-          <AiOutlineMenu />
-          <div className="hidden md:block ">
-            <Avatar />
+    <>
+      <RegisterModal />
+      <div className="relative">
+        <div className="flex flex-row items-center gap-3">
+          <div
+            onClick={() => {}}
+            className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+          >
+            Airbnb your home
+          </div>
+          <div
+            onClick={toggleOpen}
+            className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          >
+            <AiOutlineMenu />
+            <div className="hidden md:block ">
+              <Avatar />
+            </div>
           </div>
         </div>
+        {isOpen && (
+          <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+            <div className="flex flex-col cursor-pointer">
+              <>
+                <MenuItem label="Log in" onClick={() => {}} />
+                <MenuItem label="Sign up" onClick={handleOpenModalRegister} />
+              </>
+            </div>
+          </div>
+        )}
       </div>
-      {isOpen && (
-        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
-          <div className="flex flex-col cursor-pointer">
-            <>
-              <MenuItem label="Login" onClick={() => {}} />
-              <MenuItem label="Sign up" onClick={() => {}} />
-            </>
-          </div>
-        </div>
-      )}
-    </div>
+    </>
   )
 }
