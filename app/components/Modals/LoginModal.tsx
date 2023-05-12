62b4a51d741df03d68682b79019355c9a633d485
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-11 21:14:12 -0300

    feat: Created loginModal component

---
+++ app/components/Modals/LoginModal.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import { useCallback, useState } from 'react'
+import { signIn } from 'next-auth/react'
+import { AiFillFacebook, AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
+import { useAppDispatch, useAppSelector } from '@/app/redux/hooks'
+import { Modal } from './Modal'
+import { Heading } from '../Heding'
+import { Input } from '../Inputs/Input'
+import { toast } from 'react-hot-toast'
+import { Button } from '../Button'
+import { useRouter } from 'next/navigation'
+import { setStatusLoginModal } from '@/app/redux/loginModal/slice'
+import { setStatusRegisterModal } from '@/app/redux/registerModal/slice'
+
+export function LoginModal() {
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const { isOpen } = useAppSelector((state) => state.loginModalReducer)
+  const dispatch = useAppDispatch()
+  function handleCloseModal() {
+    dispatch(setStatusLoginModal(false))
+  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  function handleModalRegister() {
+    handleCloseModal()
+    dispatch(setStatusRegisterModal(true))
+  }
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true)
+    signIn('credentials', {
+      ...data,
+      redirect: false,
+    }).then((callback) => {
+      setIsLoading(false)
+
+      if (callback?.ok) {
+        toast.success('Logged in success!')
+        router.refresh()
+        handleCloseModal()
+      }
+
+      if (callback?.error) {
+        toast.error(callback.error)
+      }
+    })
+  }
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading title="Welcome back" subtitle="Login to your account!" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Facebook"
+        icon={AiFillFacebook}
+        iconColor="#3b5998"
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => signIn('google')}
+      />
+      <Button
+        outline
+        label="Continue with Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
+      <div className="flex flex-row items-center justify-center gap-2">
+        <div>{"Don't have an account?"}</div>
+        <div
+          onClick={handleModalRegister}
+          className="text-neutral-800 cursor-pointer hover:underline"
+        >
+          Register
+        </div>
+      </div>
+    </div>
+  )
+  return (
+    <Modal
+      isOpen={isOpen}
+      disabled={isLoading}
+      title="Login"
+      onClose={handleCloseModal}
+      secondaryActionLabel="Cancel"
+      actionlabel="Continue"
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    ></Modal>
+  )
+}
