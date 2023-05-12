e856d3d606d6162dbde212eb53486a1043453a77
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-11 21:15:05 -0300

    Remove actions for Redux

--- app/redux/registerModal/actions.ts
+++
@@ -1,9 +0,0 @@
-export const closeRegisterModal = () => ({
-  type: 'registerModal/status',
-  payload: false,
-})
-
-export const openRegisterModal = () => ({
-  type: 'registerModal/status',
-  payload: true,
-})
