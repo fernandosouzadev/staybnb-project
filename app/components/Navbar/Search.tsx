d5b2e30a3939cb137b49719f8253ea302b789ede
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-11 21:15:48 -0300

    Fix text

--- app/components/Navbar/Search.tsx
+++ app/components/Navbar/Search.tsx
@@ -8,7 +8,7 @@ export function Search() {
       <div className="flex flex-row items-center justify-between">
         <div className="text-sm font-semibold px-6">Anyware</div>
         <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-          any
+          Any week
         </div>
         <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
           <div className="hidden sm:block">Add Guests</div>
