7dca922a2f9a449ceafd48cf281fa3d95d7a925e
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-15 19:59:38 -0300

    feat: Created EmptyState Component

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { Button } from './Button'
+import { Heading } from './Heding'
+
+interface EmptyStateProps {
+  title?: string
+  subtitle?: string
+  showReset?: boolean
+}
+
+export function EmptyState({
+  title = 'No Exact matches',
+  subtitle = 'Try changing or removing some of your filters',
+  showReset,
+}: EmptyStateProps) {
+  const router = useRouter()
+  return (
+    <div className="h-[60vh] flex flex-col gap-2 justify-center items-center">
+      <Heading center title={title} subtitle={subtitle} />
+      <div className="w-48 mt-4">
+        {showReset && (
+          <Button
+            outline
+            label="Remove  all filters"
+            onClick={() => router.push('/')}
+          />
+        )}
+      </div>
+    </div>
+  )
+}
