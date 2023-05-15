0770f62dadf32574de92ab03558dbed1a8380891
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-15 19:59:29 -0300

    feat: Created Footer Component

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+export function Footer() {
+  const footerItens = ['Terms', 'Sitemap', 'Privacy', 'Your Privacy Choices']
+  return (
+    <div className="w-full py-5 px-20 bg-white border border-t-1 border-rose-300 fixed bottom-0 flex flex-row justify-between">
+      <div className="flex flex-row gap-2">
+        © 2023 Airbnb, Inc.{' '}
+        {footerItens.map((item) => {
+          return (
+            <div key={item} className="cursor-pointer hover:underline ">
+              · {item} ·
+            </div>
+          )
+        })}
+      </div>
+      <div>English (US) - Choose a currency - $ USD - Support & resources </div>
+    </div>
+  )
+}
