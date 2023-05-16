76e95c7adeb297ccab584532414499872c3b1a97
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-16 20:50:48 -0300

    feat: added parms in Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 import { Listing } from '@prisma/client'
 import getCurrentUser from './actions/getCurrentUser'
-import getListings from './actions/getListings'
+import getListings, { IListingsParams } from './actions/getListings'
 import { Container } from './components/Container'
 import { EmptyState } from './components/EmptyState'
 import { ListingCard } from './components/Listings/ListingCard'
 
-export default async function Home() {
-  const listings = await getListings()
+interface HomeProps {
+  searchParams: IListingsParams
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const listings = await getListings(searchParams)
   const currentUser = await getCurrentUser()
 
   if (listings.length === 0) {
     return <EmptyState showReset />
   }
+
   return (
     <Container>
       <div className="pt-24 my-5 grid grid-cols-1 sm:grid-cols-2 md-grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
