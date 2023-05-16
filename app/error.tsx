261c200fa2c2e479db3ec01940755118b95be26c
Fernando Souza <fernandosouzadev@gmail.com>
2023-05-16 20:48:16 -0300

    feat: Created page Error

---
+++ app/error.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useEffect } from 'react'
+import { EmptyState } from './components/EmptyState'
+
+interface ErrorPageProps {
+  error: Error
+}
+
+export default function ErrorPage({ error }: ErrorPageProps) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+  return <EmptyState title="Uh Oh" subtitle="Somenthing went wrong!" />
+}
